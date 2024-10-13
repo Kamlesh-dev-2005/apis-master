31a99129ffd7c9481b74fb7b31a9d16b3625a53b
Kamlesh-dev-2005 <inboxai790@gmail.com>
2024-10-13 21:54:48 +0530

    Update layout.tsx

--- Apis-Master/src/app/layout.tsx
+++ Apis-Master/src/app/layout.tsx
@@ -5,15 +5,18 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
+import Head from "next/head";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title:{
-    template:'%s Api | Api Master',
-    default:'Api Master'
+  title: {
+    template: '%s | Api Master',
+    default: 'Api Master',
   },
   description: "Explore comprehensive API documentation for developers. Find RESTful endpoints, examples, and integration guides on Apis Master. Enhance your web projects with our easy-to-use APIs.",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,13 +24,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION} />
-      <meta name="google-adsense-account" content={process.env.NEXT_PUBLIC_ADS_GOOGLE}></meta>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content={process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION}
+        />
+        <meta
+          name="google-adsense-account"
+          content={process.env.NEXT_PUBLIC_ADS_GOOGLE}
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2438225168202058"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      
       <body className={inter.className}>
         <Header />
-        <main className="flex flex-col  h-[100vh] relative">
+        <main className="flex flex-col h-[100vh] relative">
           <div className="flex flex-col">
-          {children}
+            {children}
           </div>
           <Footer />
         </main>
