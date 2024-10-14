1bfa768b90acc49fd8014086405809d1a1a52d53
Kamlesh-dev-2005 <inboxai790@gmail.com>
2024-10-14 22:40:46 +0530

    Update layout.tsx

--- Apis-Master/src/app/layout.tsx
+++ Apis-Master/src/app/layout.tsx
@@ -6,15 +6,33 @@ import Footer from "@/components/Footer";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import Head from "next/head";
-
+import Script from 'next/script';
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: {
     template: '%s | Api Master',
     default: 'Api Master',
   },
-  description: "Explore comprehensive API documentation for developers. Find RESTful endpoints, examples, and integration guides on Apis Master. Enhance your web projects with our easy-to-use APIs.",
+  description: "Explore comprehensive API documentation for developers. Find RESTful endpoints, examples, and integration guides on Api Master. Enhance your web projects with our easy-to-use APIs.",
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.apimaster.com',
+    title: 'Api Master',
+    description: 'Explore comprehensive API documentation for developers. Find RESTful endpoints, examples, and integration guides on Api Master.',
+    siteName: 'Api Master',
+    images: [
+      {
+        url: 'https://www.apimaster.com/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Api Master',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -25,6 +43,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="google-site-verification"
           content={process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION}
@@ -33,11 +53,6 @@ export default function RootLayout({
           name="google-adsense-account"
           content={process.env.NEXT_PUBLIC_ADS_GOOGLE}
         />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2438225168202058"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
       
       <body className={inter.className}>
@@ -50,6 +65,12 @@ export default function RootLayout({
         </main>
         <Analytics />
         <SpeedInsights />
+        <Script
+          async
+          defer
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2438225168202058"
+          crossOrigin="anonymous"
+        />
       </body>
     </html>
   );
